4fe891bf80873ca8929604932d8e0c0c9c35f7dd
Rowan <rowantyj@gmail.com>
2014-10-21 23:47:56 +0800

    version 211014 v2

---
+++ Android/src/com/jabber/topicline/AboutDetailFragment.java
@@ -0,0 +1,40 @@
+package com.jabber.topicline;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.jabber.R;
+
+/**
+ * Created by rowantan on 10/6/14.
+ */
+public class AboutDetailFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_detail_tab, null, false);
+
+        return rootView;
+    }
+
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //dedicate adapter to take over loading
+
+        DetailTabAdapter mAdapter = new DetailTabAdapter();
+
+
+
+
+
+    }
+
+}
---
+++ Android/src/com/jabber/topicline/TopicDetailFragment.java
@@ -0,0 +1,25 @@
+package com.jabber.topicline;
+
+import android.content.Context;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.View;
+
+
+/**
+ * Created by rowantan on 10/6/14.
+ */
+public class TopicDetailFragment extends FragmentActivity {
+
+    private ViewPager viewPager;
+    private TabPagerAdapter mAdapter;
+
+    public TopicDetailFragment() {
+
+
+    }
+
+
+}
+
